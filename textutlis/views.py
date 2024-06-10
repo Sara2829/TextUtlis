a56d5e186f21aa8968645c6342a3b6150f559f56
Akansha <akanshahajare123@gmail.com>
2024-06-10 17:34:05 +0530

    ese hi

--- textutlis/views.py
+++ textutlis/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html')#hey
 
 def aboutus(request):
     return render(request, 'AboutUs.html')
