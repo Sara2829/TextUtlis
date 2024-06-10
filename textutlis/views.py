ead2ed620ef581a5e7f8adf2715e48348eb525f5
saransh <2021bec102@sggs.ac.in>
2024-06-10 17:50:11 +0530

    hi

--- textutlis/views.py
+++ textutlis/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')#hey
+    return render(request, 'index.html')#hey;;';';
 
 def aboutus(request):
     return render(request, 'AboutUs.html')
